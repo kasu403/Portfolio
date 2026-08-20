ae31f24075f46ab8c38e7411439e62b1202171cc
Mavi <mavisberrye@gmail.com>
2026-08-19 22:10:37 -0300

    Ajustes de estrutura em monitor.jsx: adição de novos botões e divs. Ajustes de estilo em app.css: estilização dos novos botões e divs

--- src/Monitor.jsx
+++ src/Monitor.jsx
@@ -1,10 +1,25 @@
 function Monitor({children}) {
     return (
         <div className="monitorestilo">
+
+            <div className="telacontainer">
             <div className="tela">
                {children}
                <div className="scanlines"></div>
             </div>
+            </div>
+
+            <div className="painelcontrole">
+                <div className="botaodial"></div>
+                <div className="botaodial"></div>
+                <div className="ranhuras tv">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+
         </div>
     )
 }
